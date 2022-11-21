9469eca2b58a233a7205880649c00dd98f75c9c1
Devlin Sypher <devlins@advanced-ent.com>
2022-11-21 00:23:57 -0500

    rm wouldn't work without sudo permision

--- v2pUpdate.sh
+++ v2pUpdate.sh
@@ -5,4 +5,4 @@ LATEST_VERSION="v1.3.0";
 curl -L https://github.com/Virtuality360/VideoPipeline/releases/download/$LATEST_VERSION/Upload.zip > ~/Downloads/videoUploader.zip;
 sudo unzip ~/Downloads/videoUploader.zip -d ~/Downloads/;
 sudo mv ~/Downloads/Upload/upload.py /usr/local/bin/Upload/upload.py;
-rm -rf ~/Downloads/Upload.zip ~/Downloads/Upload/
+sudo rm -rf ~/Downloads/Upload.zip ~/Downloads/Upload/
