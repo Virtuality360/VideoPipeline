a35e3eeb576ea76fbfa1364c92b9a788219c9b13
Devlin Sypher <devlins@advanced-ent.com>
2022-11-21 00:13:50 -0500

    Added update script

---
+++ v2pUpdate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+LATEST_VERSION="v1.3.0";
+
+curl -L https://github.com/Virtuality360/VideoPipeline/releases/download/$LATEST_VERSION/Upload.zip > ~/Downloads/videoUploader.zip;
+sudo unzip ~/Downloads/videoUploader.zip -d ~/Downloads/;
+sudo mv ~/Downloads/Upload/upload.py /usr/local/bin/Upload/upload.py;
+rm -rf ~/Downloads/Upload.zip ~/Downloads/Upload/
